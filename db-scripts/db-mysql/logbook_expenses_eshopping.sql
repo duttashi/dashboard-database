284490738bcac7a8abf0457a1bfb62bb8ee724c1
Ashish Dutt <ashish.dutt8@gmail.com>
2021-03-28 15:17:54 +0800

    modified script
    - add new queries
    - update existing queries

--- db-scripts/db-mysql/logbook_expenses_eshopping.sql
+++ db-scripts/db-mysql/logbook_expenses_eshopping.sql
@@ -4,7 +4,7 @@ insert into tbl_eshopping (
 order_place_date, order_recv_date,
 store_name, warranty_type,
 item_name, item_desc,
-item_cost,item_weight, item_shipping_fee)
+item_cost,item_weight, item_ship_fee)
 values
 ("2020-11-11","2020-11-12","nongnong","no warranty",
 "sony mdr-xb950bt bluetooth headphone",
@@ -20,7 +20,7 @@ insert into tbl_eshopping (
 order_place_date, order_recv_date,
 store_name, warranty_type,
 item_name, item_desc,
-item_cost,item_weight, item_shipping_fee,item_discount)
+item_cost,item_weight, item_ship_fee,item_ship_fee_discount)
 values
 ("2020-12-12","2020-12-17","Ugreen","local warranty",
 "HDMI cable",
@@ -31,7 +31,7 @@ insert into tbl_eshopping (
 order_place_date, order_recv_date,
 store_name, warranty_type,
 item_name, item_desc,
-item_cost, item_discount)
+item_cost, item_ship_fee_discount)
 values
 ("2021-01-06","2021-01-15","choetech","local warranty",
 "Micro USB Cable",
@@ -40,32 +40,73 @@ values
 
 insert into tbl_eshopping (
 order_place_date, order_recv_date,
-store_name, warranty_type,
+store_name, warranty_type, warranty_time,
 item_name, item_desc,
-item_cost, item_discount)
-values
-("2021-01-06","2021-01-15","choetech","local warranty",
-"Micro USB Cable",
-"cable",
-"4.70","6");
-
-insert into tbl_eshopping (
-order_place_date, order_recv_date,
-store_name, warranty_type,
-item_name, item_desc,
-item_cost, item_shipping_fee,item_discount)
+item_cost, item_ship_fee,item_ship_fee_discount)
 values
 ("2021-01-10","2021-01-11",
-"Xiaomi","international warranty", "1 year",
+"Xiaomi Online","international", 
+"1 year",
 "Mi Power Bank 3 10000mAh",
 "power_bank",
 "35.90","4.50","6");
 
+insert into tbl_eshopping (
+order_place_date, order_recv_date,
+store_name, warranty_type, 
+item_name, item_desc,
+item_cost, item_ship_fee)
+values
+("2021-01-17","2021-01-21",
+"Milando","no warranty", 
+"3-tier rubric cube",
+"child toy",
+"3.90","4");
+
+insert into tbl_eshopping (
+order_place_date, order_recv_date,
+store_name, warranty_type, 
+item_name, item_desc,
+item_cost, item_ship_fee_discount)
+values
+("2021-01-21","2021-01-25",
+"Carote","1 year", 
+"vegtable chopper",
+"kitchen tool",
+"17.90","4.50");
+
+insert into tbl_eshopping (
+order_place_date, order_recv_date,
+store_name, warranty_type, 
+item_name, item_desc,
+item_cost, item_ship_fee_discount)
+values
+("2021-01-25","2021-02-02",
+"Medical Mall","no warranty", 
+"10-pair ear-plugs",
+"sleep",
+"3.92","6");
+
+insert into tbl_eshopping (
+order_place_date, order_recv_date,
+store_name, warranty_type, warranty_time, 
+item_name, item_desc,
+item_cost, item_ship_fee_discount)
+values
+("2021-03-18","2021-03-23",
+"Xiaomi Online","international","1 month", 
+"Enchen Blackstone 3D Electric Shaver",
+"personal care",
+"37","6");
+
+
+
+# BELOW CODE FOR CREATING TABLE
 use logbook;
 drop table if exists tbl_eshopping;
 # create table electronic_item
 create table tbl_eshopping(
-shopid int not null auto_increment,
+#shopid int not null auto_increment,
 order_website varchar(20) default "lazada.com.my",
 store_loc varchar(20) default "china",
 order_place_date date, 
@@ -77,5 +118,6 @@ item_name varchar(40), item_category varchar(20) default "electronic",
 item_desc varchar(50), 
 item_cost float default 0.0, item_weight varchar(10) default "NA",
 item_discount float default 0.0,
-item_shipping_fee float default 0.0, rating int default 0,
-primary key(shopid));
+item_ship_fee float default 0.0, item_ship_fee_discount float default 0.0, 
+rating int default 0,
+primary key(order_recv_date));
